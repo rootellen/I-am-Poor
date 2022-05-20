f9c7851d8c353a6097dbd878874945e960b2b82e
Ruth Ellen <t774832@f1rst.com.br>
2022-05-20 09:05:48 -0300

    i am poor app

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Ruth Ellen da Silva on 20/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
